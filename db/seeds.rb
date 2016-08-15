de374cfbad05cbdb22ad4f4d2d81edaa7d3debfc
Michael Stenzler <mike@nexuscafe.com>
2016-08-14 23:07:36 -0400

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([ { display_name: "Mike1", email: "mike1@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Mike2", email: "mike2@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Mike3", email: "mike3@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Mike4", email: "mike4@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Mike5", email: "mike5@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Mike6", email: "mike6@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Mike7", email: "mike7@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Mike8", email: "mike8@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Anne", email: "anne@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Anne2", email: "anne2@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Anne3", email: "anne3@example.com", password: "foobar", password_confirmation: "foobar"},
-  { display_name: "Anne4", email: "anne4@example.com", password: "foobar", password_confirmation: "foobar"},
-
+users = User.create([ 
+  { display_name: "Mike", email: "mike@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male", is_admin: true},
+  { display_name: "Mike1", email: "mike1@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male"},
+  { display_name: "Mike2", email: "mike2@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male"},
+  { display_name: "Mike3", email: "mike3@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male"},
+  { display_name: "Mike4", email: "mike4@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male"},
+  { display_name: "Mike5", email: "mike5@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male"},
+  { display_name: "Mike6", email: "mike6@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male"},
+  { display_name: "Mike7", email: "mike7@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male"},
+  { display_name: "Mike8", email: "mike8@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Male"},
+  { display_name: "Anne", email: "anne@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Female"},
+  { display_name: "Anne2", email: "anne2@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Female"},
+  { display_name: "Anne3", email: "anne3@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Female"},
+  { display_name: "Alice", email: "alice@nexuscafe.com", password: "foobar", password_confirmation: "foobar", gender: "Female"}
   ])
