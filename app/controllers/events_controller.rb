3b12bb465ef81c74dcb41b4210b12c44b4346c21
Michael Stenzler <mike@nexuscafe.com>
2016-08-16 09:30:03 -0400

    Only show upcomming events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.where('start_time > ?', DateTime.now).all
   end
 
   # GET /events/1
